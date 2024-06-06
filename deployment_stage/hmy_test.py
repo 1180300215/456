594c887468245779d178edd016f2d34fdaf760a7
23s---hmy <281536674@qq.com>
2024-06-06 19:30:24 +0800

    evaluation

--- deployment_stage/hmy_test.py
+++
@@ -1,4 +0,0 @@
-oppo_index = [0]
-agent_index = [1,2]
-obs_list_n = [None for _ in oppo_index + agent_index]
-print(len(obs_list_n))
--- deployment_stage/utils.py
+++ deployment_stage/utils.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import torch
 import wandb
+from torch import nn
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from offline_stage_2.utils import eval_episode_rtg
@@ -226,7 +227,78 @@ def collect_episodes(encoder, decoder, env_and_test_oppo, num_test, switch_inter
 
 
 
+# def load_all_data(offline_data, online_data, config_dict):
+#     obs_dim = config_dict["OBS_DIM"]
+#     act_dim = config_dict["ACT_DIM"]
+#     num_offline_oppo_policy = len(offline_data)
+#     num_online_oppo_policy = len(online_data)
+#     if config_dict["OBS_NORMALIZE"]:
+#         obs_offline_mean_list = config_dict["offline_OBS_MEAN"]
+#         obs_offline_std_list = config_dict["offline_OBS_STD"]
+#         obs_online_mean_list = config_dict["online_OBS_MEAN"]
+#         obs_online_std_list = config_dict["online_OBS_STD"]
 
+
+
+
+def load_online_data(online_data, config_dict):
+    obs_dim = config_dict["OBS_DIM"]
+    act_dim = config_dict["ACT_DIM"]
+    K = config_dict["NUM_STEPS"]  # 最大值 每个episode
+    if config_dict["OBS_NORMALIZE"]:
+        obs_online_mean_list = config_dict["online_OBS_MEAN"]
+        obs_online_std_list = config_dict["online_OBS_STD"]
+    device = config_dict["DEVICE"]
+    online_all_data = [oppo_data for oppo_data_list in online_data for oppo_data in oppo_data_list]
+
+    def fn(batch_size=100, max_len=K):
+        n_o, a, r, timesteps, mask = [], [], [], [], []
+        for k in range(batch_size):
+            traj = online_all_data[k]
+
+            n_o.append(traj['next_observations'][:max_len].reshape(1, -1, obs_dim))  # 加入一个 1*100*8
+            a.append(traj['actions'][:max_len].reshape(1, -1, act_dim))
+            r.append(traj['rewards'][:max_len].reshape(1, -1, 1))
+            timesteps.append(
+                np.arange(1, (n_o[-1].shape[1] + 1)).reshape(1, -1)
+            )  # 1- 100
+            timesteps[-1][timesteps[-1] >= (max_len + 1)] = max_len
+            tlen = n_o[-1].shape[1]
+            n_o[-1] = np.concatenate([np.zeros((1, max_len - tlen, obs_dim)), n_o[-1]], axis=1)
+            if config_dict["OBS_NORMALIZE"]:
+                obs_mean, obs_std = obs_online_mean_list[0], obs_online_std_list[0]
+                n_o[-1] = (n_o[-1] - obs_mean) / obs_std
+            a[-1] = np.concatenate([np.ones((1, max_len - tlen, act_dim)) * -10., a[-1]], axis=1)
+            r[-1] = np.concatenate([np.zeros((1, max_len - tlen, 1)), r[-1]], axis=1)
+
+            timesteps[-1] = np.concatenate([np.zeros((1, max_len - tlen)), timesteps[-1]], axis=1)
+            mask.append(np.concatenate([np.zeros((1, max_len - tlen)), np.ones((1, tlen))], axis=1))
+        n_o = torch.from_numpy(np.concatenate(n_o, axis=0)).to(dtype=torch.float32, device=device)
+        a = torch.from_numpy(np.concatenate(a, axis=0)).to(dtype=torch.float32, device=device)
+        r = torch.from_numpy(np.concatenate(r, axis=0)).to(dtype=torch.float32, device=device)
+        timesteps = torch.from_numpy(np.concatenate(timesteps, axis=0)).to(dtype=torch.long, device=device)
+        mask = torch.from_numpy(np.concatenate(mask, axis=0)).to(device=device)
+
+        return n_o, a, r, timesteps, mask
+    return fn
+
+
+def evaluation(encoder, all_data,config):
+    # print("niubi")
+    avg_pooling = nn.AvgPool1d(kernel_size=config["NUM_STEPS"])
+    batch = all_data
+    n_o, a, r, timesteps, mask = batch
+    hidden_states = encoder(
+        obs=n_o,
+        action=a,
+        reward=r,
+        timestep=timesteps,
+        attention_mask=mask,
+    )
+
+    hidden_states = avg_pooling(hidden_states.permute([0, 2, 1])).squeeze(-1)
+
+    print(hidden_states)
 
 
 def load_online_data(oppo_context_w):
